436f03f4bdaebeee3a7530e540b06a075cf1062a
Mickael Daniel <daniel.mickael@gmail.com>
2018-07-19 12:47:21 +0200

    test: add help output

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const cli = require('./lib/cli');
 const debug = require('debug')('yo-complete:index');
+const cli   = require('./lib/cli');
+const help  = require('./lib/help');
 
-module.exports = (opts = { _: [] }) => {
+const yoComplete = module.exports = (opts = { _: [] }) => {
   debug('Init CLI with', opts);
   return cli(opts);
 };
+
+yoComplete.help = help;
--- test/test-index.js
+++ test/test-index.js
@@ -1,5 +1,6 @@
 const yoComplete = require('..');
 const assert     = require('assert');
+const opts = require('140-opts')(process.argv.slice(2));
 
 describe('Simple test', () => {
   it('Returns the name of the package', () => {
@@ -9,5 +10,15 @@ describe('Simple test', () => {
   it('Returns completion stuff', () => {
     const opts = { _: ['completion'] };
     assert.equal(yoComplete(opts), 'do completion, trigger tabtab.install() here');
+  });
+
+  it('Returns help output', () => {
+    const opts = { _: ['--help'], help: true };
+    assert.equal(yoComplete(opts), yoComplete.help);
+  });
+
+  it('Returns help output even with completion arguments', () => {
+    const opts = { _: ['completion', '--help'], help: true };
+    assert.equal(yoComplete(opts), yoComplete.help);
   });
 });
